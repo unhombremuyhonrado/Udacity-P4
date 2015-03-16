4c4106cee1932729d482bbdbd93566e50c5f29f8
unhombremuyhonrado <unhombremuyhonrado@users.noreply.github.com>
2015-03-16 19:59:41 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -507,6 +507,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 //refactoring of loop in hopes this will improve performance
+  //var items = document.querySelectorAll('.mover');
   var radian = document.body.scrollTop/1250;
   var itemsLength = items.length;
   for (var i = 0; i < items.length; i++) {
@@ -549,5 +550,5 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
-  var items = document.querySelectorAll('.mover');
+  
 });
