dedb86ad721aff8f096b4367938ae31b82be1cbc
unhombremuyhonrado <unhombremuyhonrado@users.noreply.github.com>
2015-03-16 13:26:21 -0400

    Update main.js
    removed updatePositions() from bottom of js file

--- views/js/main.js
+++ views/js/main.js
@@ -556,6 +556,4 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
-  
-  updatePositions();
 });
