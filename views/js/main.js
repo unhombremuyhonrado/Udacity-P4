d05337a348b6bd18cb345eb6ba6962e0c7ae2113
unhombremuyhonrado <unhombremuyhonrado@users.noreply.github.com>
2015-03-16 20:07:08 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -549,5 +549,4 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
-  var items = document.querySelectorAll('.mover');
 });
